ab5459eeea82f17cd20b5c9a29eabf70cd2db0ba
JeanJacquesRousseau <davidpatry88@gmail.com>
2019-01-12 06:42:22 -0500

    watershed update

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,43 @@
 /*
-Author : David Patry 
+Author : David Patry
+
+Compile command :
+"g++ -Wall -o "main" "main.cpp" joystick.cc -std=c++0x -lwiringPi -lpthread $(pkg-config --libs --cflags opencv) $(pkg-config --libs --cflags harfbuzz)"
+Run command : "./main"
+
 */
-
-
 #include "seed.cpp"
+#include "joystick.hh"
+#include <unistd.h>
+#include <string.h>
 
+//Faire cd ./servod dans "/home/pi/servoblaster/PiBits/ServoBlaster/user"
+//"echo P1-12=[60,160] > /dev/servoblaster"
 /** Global Variables */
+uint8_t choix;
 int thresh = 140;
 int max_thresh = 255;
 uint32_t nbGraine=0;
 uint32_t nccomps=0;
+bool modeAuto = true;
 
 //Motor configs
 #define motorPin	15
 #define motorD1		14
 #define motorD2		18
 uint32_t PWM = 0;
+int servoCommand =0;
+int servoInput = 130;
+int motorUpdate = 200;
+
+//PS3 Controller
+Joystick joystick("/dev/input/js0");
 
 //Screen Resolution
-#define HEIGHT	660//768//600//768
-#define WIDTH	880//1024//800//1024
+#define HEIGHT	600//768
+#define WIDTH	800//1024
 
+//Vector of seeds containing x,y positions and seed type
 vector<Point3f> seeds;
 
 
@@ -30,43 +47,81 @@ vector<Point3f> seeds;
 // pwmSetRange()
 
 
-void keyboardControl(){
-		
-	if ((char)waitKey(1)=='r'){ 
-		cout << "Voici ou elle sont :" <<endl << seeds << endl;
-		system("echo 0=60 > /dev/servoblaster");
-		cout << "Pis combien delements dans mon tableau : " << (int)seeds.size() << endl;
-		//cout << "PWM : " << PWM << endl;
-	}
-	
-	if ((char)waitKey(1) =='f') {
-		if(PWM <= 95){
-			//PWM = PWM+5;
-			system("echo 0=110 > /dev/servoblaster");
-			//softPwmWrite(motorPin,PWM);	
-			//cout << "Duty cycle : "<<PWM<<"%" <<endl;
-		}
-		else cout << "Tu as atteint la vitesse maximal pour le convoyeur" << endl;
-	}
-	
-	if ((char)waitKey(1) =='s'){
-		if(PWM >= 5){
-			//PWM = PWM-5;
-			//softPwmWrite(motorPin,PWM);				
-			//cout << "Duty cycle : "<<PWM<<"%" <<endl;
-		}
-		else cout << " Il n'y a pas de reculons pour le convoyeur" << endl;
-	}
 
+long maper(long x, long in_min, long in_max, long out_min, long out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-int main(int argv, char* argc[])
-{
+
+void keyboardControl(uint8_t choix ){
+
+
+    switch(choix){
+
+	case 'r':
+	    cout << "Voici ou elle sont :" <<endl << seeds << endl;
+	    system("echo P1-12=160 > /dev/servoblaster");
+	    cout << "Pis combien delements dans mon tableau : " << (int)seeds.size() << endl;
+	    //cout << "PWM : " << PWM << endl;
+	    break;
+
+	case 'f':
+	    if(PWM <= 95){
+	    //PWM = PWM+5;
+	    system("echo P1-12=110 > /dev/servoblaster");
+	    //softPwmWrite(motorPin,PWM);
+	    //cout << "Duty cycle : "<<PWM<<"%" <<endl;
+	    }
+	    else cout << "Tu as atteint la vitesse maximal pour le convoyeur" << endl;
+	    break;
+
+	case 's':
+	    if(PWM >= 5){
+	    //PWM = PWM-5;
+	    //softPwmWrite(motorPin,PWM);
+	    //cout << "Duty cycle : "<<PWM<<"%" <<endl;
+	    }
+	    else cout << " Il n'y a pas de reculons pour le convoyeur" << endl;
+	case 32:
+	    modeAuto=!modeAuto;
+	    if(modeAuto == 0){
+		cout << "Mode Manuel activee" << endl;
+		//cap.release();
+		if (!joystick.isFound()){
+		    printf("open failed.\n");
+		    exit(1);
+		}
+
+	    }
+	    else{
+		cout << "Mode Automatique activee" << endl;
+		//VideoCapture cap(0);
+		//cap.set(CV_CAP_PROP_FRAME_WIDTH,WIDTH);		//1024
+		//cap.set(CV_CAP_PROP_FRAME_HEIGHT,HEIGHT);	//768
+	    }
+
+	break;
+
+	default: break;
+}
+
+}
+int main(int argv, char* argc[]){
     //! [mat]
     Mat frame, frame_original, frame_morph, frame_gray, frame_threshold, frame_label;
-    Mat element = getStructuringElement( MORPH_RECT, Size( 15, 15 ), Point(-1,-1) );// Size(6,6) marche mais on a un probleme avec le glare
+    Mat element = getStructuringElement( MORPH_RECT, Size( 11, 11 ), Point(-1,-1) );// Size(6,6) marche mais on a un probleme avec le glare
 
     Seed seed;
+    Mat watershed;
+    Mat eclatee = imread(argc[1]);
+
+
+    // Ensure that it was found and that we can use it
+
+
+
+
     /*
     seed.setCenter(2,4);
     seed.setType(MAIS);
@@ -74,6 +129,7 @@ int main(int argv, char* argc[])
     cout << "Le type de seed est : " << seed.getType() << endl;
     */
     //! [motors]
+
     //wiringPiSetup();
 	//pinMode(motorPin, PWM_OUTPUT);
 	//pinMode(motorD1, OUTPUT);
@@ -85,68 +141,137 @@ int main(int argv, char* argc[])
 	//softPwmWrite(motorPin,PWM);
 	//softServoSetup(0,1,2,3,4,5,6,7);
 	//softServoWrite(0,500);
-	
+
     //! [cap]
     VideoCapture cap(0);
     cap.set(CV_CAP_PROP_FRAME_WIDTH,WIDTH);	//1024
     cap.set(CV_CAP_PROP_FRAME_HEIGHT,HEIGHT);	//768
-     
+
     //! [window]
-    namedWindow("Video Capture", WINDOW_AUTOSIZE);
+    //namedWindow("Video Capture", WINDOW_AUTOSIZE);
     namedWindow("Keyboard Control", WINDOW_AUTOSIZE);
    // namedWindow("Object Detection", WINDOW_AUTOSIZE);
-    
+
+   system("sudo /home/pi/servoblaster/PiBits/ServoBlaster/user/servod");
+
     //! [performance measurement]
 	struct timespec start, end;	// initiate start and ends values of clock
 	clock_gettime(CLOCK_REALTIME, &start); //starts clock
 	uint32_t nbLoop=0;
-	
-    while((char)waitKey(1)!=27){	// ~26 frames/secs with no algorithm and no show
 
-        cap>>frame;         
-        if(frame.empty()) break;
-	imshow("Centroid Detection",frame);
-	
-          
+    while((char)choix != 27){	// ~26 frames/secs with no algorithm and no show
 
-		//! Background extraction
-        cvtColor( frame, frame_gray, COLOR_BGR2GRAY );	//Grey scale convertion
-        threshold(frame_gray, frame_threshold, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);  //Otsu thresholding
-        morphologyEx(frame_threshold, frame_morph, MORPH_OPEN , element);	// Noise filter
-        
-	seeds=seed.identifier(frame_morph);	// Input a masked image where backgrounds has been extracted and output vector of floats with element (centroid_x, centroid_y, type)
-	//seed.sort(seeds);  // Takes in vector of seeds and do motor control to sort
-	frame_label = seed.draw(seeds, frame_morph,frame);	// TODO : Issue with drawing random centroids points on side of screen
-	//seed.printCount(seeds);	// TODO : work to be done here.
-	/*	
-	if(nbGraine != seeds.size()){	//check if a theres a change in # of seeds in the frame and tell how many there is
-		cout << "Voici le nombre de graine : " << seeds.size() << endl;	//remove background, returns number of seed detected
-		nbGraine= seeds.size();
+        if(modeAuto){
+            cap>>frame;
+            if(frame.empty())
+		{
+		    cout << " sa marche pas" << endl;
+	            break;
+	        }
+	    imshow("Keyboard Control",frame);
+
+          //! Background extraction
+	    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );	//Grey scale convertion
+	    threshold(frame_gray, frame_threshold, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);  //Otsu thresholding
+	    morphologyEx(frame_threshold, frame_morph, MORPH_OPEN , element);	// Noise filter
+	    frame.copyTo(watershed, frame_morph);
+	    //watershed = bitwise_and(frame,frame,frame_morph);
+	    imshow("mask", watershed);
+	    seeds=seed.identifier(eclatee);	// Input a masked image where backgrounds has been extracted and output vector of floats with element (centroid_x, centroid_y, type)
+	    //seed.sort(seeds);  // Takes in vector of seeds and do motor control to sort
+	    frame_label = seed.draw(seeds, frame_morph,frame);	// TODO : Issue with drawing random centroids points on side of screen
+	    //seed.printCount(seeds);	// TODO : work to be done here.
+	
+	    if(nbGraine != seeds.size()){	//check if a theres a change in # of seeds in the frame and tell how many there is
+		    //cout << "Voici le nombre de graine : " << seeds.size() << endl;	//remove background, returns number of seed detected
+		    //nbGraine= seeds.size();
+	    }
+
+
+
+	         imshow("Centroid Detection",frame_label);
+	         nbLoop++;	/// Loop counter
+
+    }else
+    {
+
+	usleep(1000);
+	JoystickEvent event;
+	if (joystick.sample(&event)){
+	    if (event.isButton()){
+		printf("Button %u is %s\n",
+		event.number,
+		event.value == 0 ? "up" : "down");
+	    }
+	    else if (event.isAxis()){
+	    //printf("Axis %u is at position %d\n", event.number, event.value);
+		if(event.number ==2){
+		    servoCommand = maper(event.value,-32767,32767,-2,2);
+		    servoCommand = servoCommand*(-1);
+
+		}
+
+		if(servoInput>59 && servoInput <201) servoInput += servoCommand;
+
+		if(servoInput > 200) servoInput = 200;
+		if(servoInput < 60) servoInput =60;
+		//cout << "servo Command : " << servoInput << endl;
+		string joystickIN = to_string(servoInput);
+		const char* pchar = joystickIN.c_str();
+
+		char buffer[256]; // <- danger, only storage for 256 characters.
+
+
+
+
+		char* motor = "echo P1-12=";// +" > /dev/servoblaster";
+		strncpy(buffer, motor, sizeof(buffer));
+		strncat(buffer, pchar, sizeof(buffer));
+
+
+		char* echo = " > /dev/servoblaster";
+		strncat(buffer, echo, sizeof(buffer));
+		cout << "motor : " << buffer << endl;
+		if(millis() - motorUpdate > 100){
+		    motorUpdate = millis();
+		    system(buffer);
+		}
+		//cout << echo << endl;
+
+	    }
 	}
-	*/
-		
-	keyboardControl();
-	imshow("Centroid Detection",frame_label);
-		
+
+
+
+    }
+
+
+
         //! [show]
 	//imshow("Video Capture",frame_morph);
-	  
-        nbLoop++;	/// Loop counter
+
+        //cout << "mode Auto : " << modeAuto << endl;
+	choix = waitKey(1);
+	keyboardControl(choix);
+	//cout << "voici ton choix" << choix << endl;
+
+
     }
-    
-    //! [performance measurements] 
+
+    //! [performance measurements]
     clock_gettime(CLOCK_REALTIME, &end);
     double difference = (end.tv_sec -start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec)/1000000000.0;
     cout << "Ca prend " << difference << "secondes pour faire " << nbLoop << " capture dimage et traitement" << endl;
-    cout << "Moyenne de : " << nbLoop/difference << " capture&traitement/secondes " << endl; 
-    
+    cout << "Moyenne de : " << nbLoop/difference << " capture&traitement/secondes " << endl;
+
     //! [Write Results]
     //imwrite("/home/pi/seed_sorter/image/otsu_thresholding.jpg",frame_threshold);
-    imwrite("/home/pi/seed_sorter/image/identified_seeds.jpg",frame_label); 
-    //imwrite("/home/pi/seed_sorter/image/noise_filtered.jpg",frame_morph); 
+    //imwrite("/home/pi/seed_sorter/image/identified_seeds.jpg",frame_label);
+    imwrite("/home/pi/seed_sorter/test/watershedEXP2.jpg",watershed);
     //imwrite("/home/pi/seed_sorter/image/gray_scaled.jpg",frame_gray);
-    //imwrite("/home/pi/seed_sorter/image/original_frame1.jpg",frame_original);  
-    
-    //cap.release();
+    imwrite("/home/pi/seed_sorter/image/watershed.jpg",frame);
+
+
+    cap.release();
     return 0;
 }
---
+++ test/seed.h
@@ -0,0 +1,61 @@
+/*
+ * Seed.h
+ * Created : 2018-17-11 23:03:22
+ * Author: David Patry
+ */
+ 
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/opencv.hpp"
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <wiringPi.h>
+//#include <softPwm.h>
+#include <softServo.h>
+
+using namespace std;
+using namespace cv;
+
+#ifndef __SEED_H__
+#define __SEED_H__
+
+enum Graine { NOT_IDENTIFIED, MAIS, SOYA }; 		
+
+class Seed
+{
+	public:
+		Seed();
+		~Seed();
+		void setCenter(float x, float y);
+		void setType(Graine sorte);
+		float getPosX();
+		float getPosY();
+		Graine getType();
+		
+		vector<Point3f> identifier(Mat mask);
+		Mat draw(vector<Point3f> seeds , Mat mask, Mat frame);
+		void sort(vector<Point3f> seeds);
+		void printCount(vector<Point3f> seeds);
+		
+		
+	protected:
+	private:
+		bool armed_;
+		uint32_t timer_;
+		float center_x;
+		float center_y;
+		Graine sorte_;
+		uint32_t nbGraine_;
+		Mat labels_;
+		Mat centroids_;
+		Mat stats_;
+		//Point2f point;	
+};
+
+
+
+#endif
+
+
