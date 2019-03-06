29f7a9da0048f1f7cbda25a087fb5f400cd46618
JeanJacquesRousseau <davidpatry88@gmail.com>
2019-03-06 11:57:41 -0500

    Added matlab directory for hog descricriptor and svm model

---
+++ matlab/computeFeatures.m
@@ -0,0 +1,59 @@
+function [ featureVectors ] = computeFeatures( im, imageBW, sideLength)
+%COMPUTEHOG Summary of this function goes here
+%   Detailed explanation goes here
+
+%% Init
+% defaults
+cellSize = [8, 8];
+blockSize = [2, 2];
+blockOverlap = ceil(blockSize/2);
+numBins = 9;
+
+
+blocksPerImage = floor(([sideLength, sideLength]./cellSize - blockSize)./(blockSize - blockOverlap) + 1);
+hogN = prod([blocksPerImage, blockSize, numBins]);
+
+%% Compute
+
+imCC = bwconncomp(imageBW);
+labels = labelmatrix(imCC);
+
+bbs = regionprops(imCC, 'BoundingBox');
+orientations = regionprops(imCC, 'Orientation');
+
+featureVectors = zeros(hogN, imCC.NumObjects);
+
+if imCC.NumObjects >= 1
+    for i=1:imCC.NumObjects
+        bb = bbs(i).BoundingBox;
+        x = round(bb(1));
+        y = round(bb(2));
+        if bb(3) > bb(4)
+            padding = floor((bb(3) - bb(4))/2);
+            y = max(y - padding, 1);
+            bb(4) = bb(4) + 2*padding;
+        elseif bb(4) > bb(3)
+            padding = floor((bb(4) - bb(3))/2);
+            x = max(x - padding, 1);
+            bb(3) = bb(3) + 2*padding;
+        end
+        subI = im(y:(y+bb(4)), x:(x+bb(3)), :);
+        subL = labels(y:(y+bb(4)), x:(x+bb(3)));
+        mask = subL == i;
+        mask(:,:,2) = mask(:,:,1);
+        mask(:,:,3) = mask(:,:,1);
+        
+        maskedSubI = subI;
+        maskedSubI(~mask) = 0;
+        
+        maskedSubI = imrotate(maskedSubI, -orientations(i).Orientation);
+        s = sideLength/size(maskedSubI,1);
+        maskedSubI = imresize(maskedSubI, s);
+        
+        
+        featureVectors(:, i) = extractHOGFeatures(maskedSubI);
+    end
+end
+
+end
+
---
+++ matlab/testing.m
@@ -0,0 +1,145 @@
+clear all
+clc
+close all
+%% Params
+corn = imread('MaisOnly.jpg');
+soy = imread('SoyaOnly.jpg');
+sideLength = 60;
+
+%% Init
+% defaults
+cellSize = [8, 8];
+blockSize = [2, 2];
+blockOverlap = ceil(blockSize/2);
+numBins = 9;
+
+
+blocksPerImage = floor(([sideLength, sideLength]./cellSize - blockSize)./(blockSize - blockOverlap) + 1);
+hogN = prod([blocksPerImage, blockSize, numBins]);
+
+%% Corn
+cornT = imbinarize(rgb2gray(corn),graythresh(rgb2gray(corn)));
+cornT = bwareaopen(cornT, 100);
+imshow(cornT)
+cornS = corn;
+
+mask = cat(3, cornT, cornT, cornT);
+cornS(mask == 0) = 0;
+imshow(cornS)
+
+%% Soy
+soyT = imbinarize(rgb2gray(soy),graythresh(rgb2gray(soy)));
+soyT = bwareaopen(soyT, 100);
+
+% Testing area filtering
+% Because soys are touching each others
+% TODO: Takes photos without touching soys
+soyT = bwareafilt(soyT, [100, 650]);
+
+%
+imshow(soyT)
+soyS = soy;
+
+mask = cat(3, soyT, soyT, soyT);
+soyS(mask == 0) = 0;
+imshow(soyS)
+
+% Pad array because soy are too close to border
+% TODO: Takes photos with no soy on borders
+
+soyT = padarray(soyT, [10,10]);
+soy = padarray(soy, [10,10]);
+
+%% Compute vectors
+
+cornVecs = computeFeatures(corn, cornT, sideLength);
+soyVecs = computeFeatures(soy, soyT, sideLength);
+
+%% Prepare data for learning
+
+data = [cornVecs', zeros(size(cornVecs,2), 1); soyVecs', ones(size(soyVecs,2), 1)];
+
+%% Training
+trainedModel = trainClassifierTest(data);
+
+%% Validate
+load('trainedModelLinearSVM')
+mixed = imread('mix1.jpg');
+mixedT = imbinarize(rgb2gray(mixed),graythresh(rgb2gray(mixed)));
+mixedT = bwareaopen(mixedT, 300);
+% imshow(mixedT)
+% mixedS = mixed;
+% 
+% mask = cat(3, mixedT, mixedT, mixedT);
+% mixedS(mask == 0) = 0;
+% imshow(mixedS)
+
+vecs = computeFeatures(mixed, mixedT, sideLength);
+
+imCC = bwconncomp(mixedT);
+centroids = regionprops(imCC, 'Centroid');
+labels = labelmatrix(imCC);
+
+fit = trainedModel.predictFcn(vecs');
+
+% results = label2rgb(labels);
+
+texts = cell(imCC.NumObjects,1);
+positions = zeros(imCC.NumObjects, 2);
+
+for i = 1:imCC.NumObjects
+    c = centroids(i).Centroid;
+    c = round(c);
+    if fit(i) == 0
+        texts{i} = 'Corn';
+    else
+        texts{i} = 'Soy';
+    end
+    positions(i,:) = c;
+end
+
+results = insertText(label2rgb(labels), positions, texts);
+imshow(results);
+imwrite(results, 'results.jpg')
+
+
+%% Testing with chain
+load('trainedModelLinearSVM')
+mixed = imread('grain_avec_chaine.jpg');
+mixedT = imbinarize(rgb2gray(mixed),graythresh(rgb2gray(mixed)));
+mixedT = bwareaopen(mixedT, 300);
+
+mixedHSV = rgb2hsv(mixed);
+mask = mixedHSV(:,:,2) < 0.1;
+mask(:,:,2) = mask(:,:,1);
+mask(:,:,3) = mask(:,:,1);
+
+mixedHSV_masked = mixedHSV;
+mixedHSV_masked(mask) = 0;
+imshow(mixedHSV_masked)
+
+% vecs = computeFeatures(mixed, mixedT, sideLength);
+% 
+% imCC = bwconncomp(mixedT);
+% centroids = regionprops(imCC, 'Centroid');
+% labels = labelmatrix(imCC);
+% 
+% fit = trainedModel.predictFcn(vecs');
+% 
+% texts = cell(imCC.NumObjects,1);
+% positions = zeros(imCC.NumObjects, 2);
+% 
+% for i = 1:imCC.NumObjects
+%     c = centroids(i).Centroid;
+%     c = round(c);
+%     if fit(i) == 0
+%         texts{i} = 'Corn';
+%     else
+%         texts{i} = 'Soy';
+%     end
+%     positions(i,:) = c;
+% end
+% 
+% results = insertText(label2rgb(labels), positions, texts);
+% imshow(results);
+% imwrite(results, 'results_chain.jpg')
